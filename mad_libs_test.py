5ac5d01f2fd9c87a5e801b46bc7968e0808c6c92
Wailingmist <lumanwiller@gmail.com>
2021-12-06 14:31:22 -0500

    test message text

---
+++ mad_libs_test.py
@@ -0,0 +1,12 @@
+#MAD LIBS TEST - LEARNING PYTHON - DECEMBER 2021:
+noun1 = input("Noun: ")
+adj1 = input("Adjective: ")
+subject1 = input("School Subject: ")
+place1 = input("Location: ")
+famous_person1 = input("Famous Person: ")
+verb1 = input("Verb: ")
+
+madlib = f"My name is {noun1}.  I am feeling quite {adj1} today.  I would like to learn {subject1} because I want to " \
+         f"visit {place1} someday.  My best friend's name is {famous_person1}.  We {verb1} together.  Thank you."
+
+print(madlib)
